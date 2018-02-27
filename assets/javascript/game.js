6645c3e9e36d032c40720fb9b92f8df6387dcec8
unknown <leabney@gmail.com>
2018-02-27 17:04:02 -0600

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,122 @@
 $(document).ready(function () {
 
-    var character = [];
-    var numberCharacters = $(".character").length;
     var yourCharacter = "";
-    var yourCharacterHTML;
-    var yourAttackPower;
+    var yourHealthPoints;
+    var yourAttackPoints;
+    var yourCard;
+    var yourDisplayName;
 
-    var enemies;
-    var enemiesHTML;
     var defender = "";
-    var defenderAttackPower;
+    var defenderHealthPoints;
+    var defenderCounterAttackPoints;
+    var defenderDisplayName;
 
+    function Character(name, displayName, imgSrc) {
+        this.name = name;
+        this.displayName = displayName;
+        this.type = "character";
+        this.healthPoints = Math.round((Math.floor(Math.random() * 70) + 101) / 10) * 10;
+        this.attackPoints = Math.floor(Math.random() * 14) + 1;
+        this.counterAttackPoints = Math.floor(Math.random() * 24) + 1;
+        this.imgSrc = imgSrc;
+        this.card =
+            '<div class="card img-thumbnail character" id="' + this.name + '">' +
+            '<div class="card-block">' +
+            '<p class="card-text text-center">' + this.displayName + '</p>' +
+            '</div>' +
+            '<img class="img-responsive" src="' + this.imgSrc + '" alt="' + this.displayName + '">' +
+            '<div class="points">' + this.healthPoints + '</div>' +
+            '</div>';
+    }
 
-    for (var i = 0; i < numberCharacters; i++) {
-        character[i] = {
-            name: $(".character:eq(" + i + ")").attr("id"),
-            type: "character",           
-            healthPoints: Math.round((Math.floor(Math.random() * 70) + 101)/10)*10,
-            attackPower: Math.floor(Math.random() * 4) + 1,
-            counterAttackPower: Math.floor(Math.random() * 14) + 11
-        };
+    var obi_wan = new Character("obi_wan", "Obi Wan Kenobi", "assets/images/obi_wan.jpg");
+    var luke_skywalker = new Character("luke_skywalker", "Luke Skywalker", "assets/images/luke_skywalker.jpg");
+    var darth_sidious = new Character("darth_sidious", "Darth Sidious", "assets/images/darth_sidious.jpg");
+    var darth_maul = new Character("darth_maul", "Darth Maul", "assets/images/darth_maul.jpg");
 
-        $(".character:eq(" + i + ")").find(".points").html(character[i].healthPoints)
-        yourAttackPower = character[i].attackPower;
+    var characters = { obi_wan, luke_skywalker, darth_sidious, darth_maul };
 
-    };
-    
-    if (yourCharacter === "") {
-        $(".card").click(function () {
-            yourCharacterHTML = '<div class="card img-thumbnail" id="yourCharacter">' + $(this).html();
-            yourCharacter = $(this).attr("id");
-            console.log("Your Character: " + yourCharacter);
+    $(".characters").html(obi_wan.card + luke_skywalker.card + darth_sidious.card + darth_maul.card);
 
-            $(this).remove();
-            $(".enemies").html($(".characters").html());
-            $(".characters").remove();
+    $(".card").click(function () {
+        if (yourCharacter === "") {
+            for (var key in characters) {
+                if (characters.hasOwnProperty(key)) {
+                    var character = characters[key];
 
-            $(".yourCharacter").html(yourCharacterHTML);
-            for (var i = 0; i < numberCharacters; i++) {
-                if (character[i].name === $(this).attr("id")) {
-                    character[i].type = "yourCharacter";
-                    console.log(yourCharacterHTML);
-                }
-                else {
-                    character[i].type = "enemy";
-                    if (defender === "") {
-                        $(".card").click(function () {
-                            defenderHTML = '<div class="card img-thumbnail" id="defender">' + $(this).html();
-                            defender = $(this).attr("id");
-                            console.log("Defender: " + defender);
-                            
-                            $(this).remove();
-                            $(".defender").html(defenderHTML);
+                    if (character.name === $(this).attr("id")) {
+                        yourCharacter = $(this).attr("id");
+                        yourDisplayName=character.displayName;
+                        yourHealthPoints = character.healthPoints;
+                        yourAttackPoints = character.attackPoints;
+                        $(".yourCharacter").html(character.card);
 
+                        console.log("Your character: " + character.displayName);
+                        console.log("Your health points: " + yourHealthPoints);
+                        console.log("Your attack points: " + yourAttackPoints);
 
-                            $(".yourCharacter").html(yourCharacterHTML);
-                            for (var i = 0; i < numberCharacters; i++) {
-                                if (character[i].name === $(this).attr("id")) {
-                                    character[i].type = "defender";
-                                    console.log(defender);
-                                    defenderAttackPower=character[i].counterAttackPower;
-                                }
-                                else {
-                                    character[i].type = "enemy";
-
-
-                                };
-                            };
-
-                        });
+                        $(this).remove();
+                        $(".characters").appendTo($(".enemies"))
+                       
+                        
                     }
-
-                };
-            };
-
-        });
-
-    };
-
-    $(".btn").click(function(){
-        if (yourCharacter !="" && defender !=""){
-            $(".attackMessage").html(("You attacked "+ defender.replace("_"," ") + " for " + yourAttackPower + " damage points. <br>" + defender.replace("_"," ") + " attacked you for " + defenderAttackPower + " damage points." ).toUpperCase());
-            alert(yourCharacterHTML);
+                }
+            }
         }
-        else {
-           alert("Please select your character and a defender before attacking.");
+
+
+        
+        if ($(this).attr("id") !== yourCharacter && defender === "") {
+
+            for (var key in characters) {
+                if (characters.hasOwnProperty(key)) {
+                    var character = characters[key];
+
+                    if (character.name === $(this).attr("id")) {
+                        defender = $(this).attr("id");
+                        defenderDisplayName = character.displayName;
+                        defenderHealthPoints = character.healthPoints;
+                        defenderCounterAttackPoints = character.attackPoints;
+                        $(".defender").html(character.card);
+
+
+                        console.log("Defender: " + character.displayName);
+                        console.log("Defender health points: " + defenderHealthPoints);
+                        console.log("Defender attack points: " + defenderCounterAttackPoints);
+
+                        $(this).remove();
+                    }
+                }
+            }
         }
     });
+    
+    $(".btn").click(function(){
+        if (yourCharacter !="" && defender !=""){
+
+            if (yourHealthPoints > 0 && defenderHealthPoints>0){
+
+            yourHealthPoints=yourHealthPoints-defenderCounterAttackPoints;
+            defenderHealthPoints=defenderHealthPoints-yourAttackPoints;
+            $(".yourCharacter").find(".points").html(yourHealthPoints);
+            $(".defender").find(".points").html(defenderHealthPoints);
+            $(".attackMessage").html("You attacked " + defenderDisplayName + " for "  + yourAttackPoints + " points. <br> " + defenderDisplayName + " attacked you for " + defenderCounterAttackPoints + " points.");
+
+        }
+
+            if(yourHealthPoints <1) {
+                $(".attackMessage").html("You have been killed by " + defenderDisplayName +".");
+            }
+
+            if (defenderHealthPoints < 1) {
+                $(".attackMessage").html("You defeated "+ defenderDisplayName + ".");
+            }
+        
+    
+    }
 
 
-    //check//
-    console.log(character);
+
 });
 
-
-
+});
